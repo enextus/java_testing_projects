cfbadc09002af32d4c5629c3a5cc199371f4ce52
enextus <enextus@gmail.com>
2021-06-01 10:56:23 +0200

    01

--- 21_06_01_test_001/src/main/java/com/telran/qa14/FirstSelenimTest.java
+++ 21_06_01_test_001/src/main/java/com/telran/qa14/FirstSelenimTest.java
@@ -3,15 +3,23 @@ package com.telran.qa14;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class FirstSelenimTest {
 
     WebDriver driver;
 
-    // befor - setUp(){}
+    //before - setUp(){}
     @BeforeMethod
     public void setUp() {
         driver = new ChromeDriver();
-        // System.setProperty("webdriver.chrome.driver", "Path...../chromedriver")
+        //System.setProperty("webdriver.chrome.driver", "Path...../chromedriver")
+        driver.get("https://www.google.com/");
+    }
+
+    //Test
+    @Test
+    public void openSite() {
+        System.out.println("site opened!");
     }
 }
