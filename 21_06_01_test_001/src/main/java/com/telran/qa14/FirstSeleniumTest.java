c7696721245f019927524220c872161b6dce2c05
enextus <enextus@gmail.com>
2021-06-01 11:27:47 +0200

    01

--- 21_06_01_test_001/src/main/java/com/telran/qa14/FirstSeleniumTest.java
+++ 21_06_01_test_001/src/main/java/com/telran/qa14/FirstSeleniumTest.java
@@ -1,7 +1,11 @@
 package com.telran.qa14;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -22,10 +26,26 @@ public class FirstSeleniumTest {
 
     }
 
-    //Test
+/*    //Test
     @Test
     public void openSite() {
         System.out.println("site opened!");
+    }*/
+
+    //Test
+    @Test
+    public void searchProductTest() {
+        driver.findElement(By.name("search_query")).sendKeys("summer dresses" + Keys.ENTER);
+        String text = driver.findElement(By.className("lighter")).getText();
+
+        Assert.assertEquals(text, "\"SUMMER DRESSES\"");
     }
+
+    //after - tearDown()
+    @AfterMethod(enabled = false)
+    public void tearDown() {
+        driver.quit();
+    }
+
 
 }
