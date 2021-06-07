f8e5d03da76e055a682cc2acb1ae3f816e009f49
enextus <enextus@gmail.com>
2021-06-07 13:15:15 +0200

    fsdfsdf

--- 21_06_07_find_some_elements/src/main/java/findTemsOfTabTest.java
+++ 21_06_07_find_some_elements/src/main/java/findTemsOfTabTest.java
@@ -7,26 +7,31 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.NoSuchElementException;
 import java.util.concurrent.TimeUnit;
 
 class findTermsOfTabTest {
 
-
-    WebDriver driver;
+    WebDriver driver = new ChromeDriver();
 
     @BeforeMethod
     public void setUp() {
-        //System.setProperty("webdriver.chrome.driver","path/chromedriver.exe");
-        //driver = new ChromeDriver();
-        WebDriver driver = new ChromeDriver();
         driver.get("https://ilcarro-dev-v1.firebaseapp.com/");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 
-    @BeforeMethod
     public boolean isFindCarFormPresent(By by) {
         return driver.findElements(by).size() > 0;
+    }
+
+    public boolean isElementPresent(By locator) {
+        try {
+            driver.findElement(locator);
+            return true;
+        } catch (NoSuchElementException ex) {
+            return false;
+        }
     }
 
     @Test
@@ -39,25 +44,22 @@ class findTermsOfTabTest {
 //
         wd.findElement(By.xpath("//ul[@class='header__nav desktop']/li[3]")).click();
 
-        //Assert.assertTrue(isElementPresent(By.xpath("//div[contains(.,'Terms')]")));
-        Assert.assertTrue(isElementPresent(By.cssSelector(".search-sidebar__content")));
+        Assert.assertTrue(isElementPresent(By.xpath("//div[contains(.,'Terms')]")));
+        //Assert.assertTrue(isElementPresent(By.cssSelector(".search-sidebar__content")));
     }
 
-    private boolean isElementPresent(By cssSelector) {
-    }
 
-    @Test
+/*    @Test
     public void findSearchTest() {
-
         driver.findElement(By.xpath("//ul[@class='header__nav desktop']/li[1]")).click();
         System.out.println(driver);
         Assert.assertTrue(isElementPresent(By.cssSelector(".search-sidebar__content")));
-    }
-
+    }*/
 
     @AfterMethod(enabled = true)
     public void tearDown() {
         System.out.println("Driver: " + driver.getTitle());
         driver.quit();
     }
+
 }
