04fabcd335a366e4dbb9da29c7586e9b201936cb
enextus <enextus@gmail.com>
2021-06-08 11:15:35 +0200

    asdas asd

---
+++ 21_06_08_ilCarro/src/main/java/com/ilCarro/qa14/OpenHomePageTest.java
@@ -0,0 +1,13 @@
+
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class OpenHomePageTest extends TestBase {
+
+    @Test
+    public void homePageTest() {
+        System.out.println("FindCarForm: " + isElementPresent(By
+                .cssSelector(".Main_mainpage__find_your_car__AHLkww")));
+    }
+
+}
