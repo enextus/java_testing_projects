88903231a38dc9d64f6e9f28b709bcfe0aa21dd3
enextus <enextus@gmail.com>
2021-06-09 08:26:18 +0200

    sdfsdf sdf s

--- 21_06_08_ilCarro/src/main/java/com/ilCarro/qa14/CreateAccountTest.java
+++ 21_06_08_ilCarro/src/main/java/com/ilCarro/qa14/CreateAccountTest.java
@@ -1,42 +1,32 @@
 package com.ilCarro.qa14;
 
-import com.ilCarro.qa14.TestBase;
-import org.openqa.selenium.By;
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class CreateAccountTest extends TestBase {
-    //preconditions: user should be logged out
+
     @BeforeMethod
     public void ensurePrecondition() {
-        if (!isSignUpFormPresent()) {
+        if (!isSignUpButtonPresent()) {
             logout();
         }
     }
 
     @Test
     public void signUpTest() {
-        click(By.cssSelector("[href='/signup']"));
-        isLoginFormPresent();
+        clickOnSignUpTab();
+        isSignUpFormPresent();
 
         fillRegistrationForm(new User()
                 .withFirstName("Mats")
                 .withSecondName("Hummels")
-                .withEmail("hummels2@gmail.com")
+                .withEmail("hummels3@gmail.com")
                 .withPassword("Hummels098765"));
 
-        click(By.cssSelector("#check_policy"));
+        clickCheckPolicy();
         submit();
-        Assert.assertTrue(isElementPresent(By.cssSelector(".Login_login__right_block__1niYm")));
+        isLogInFormPresent();
 
-    }
-
-    public void fillRegistrationForm(User user) {
-        type(By.cssSelector("#first_name"), user.getFirstName());
-        type(By.cssSelector("#second_name"), user.getSecondName());
-        type(By.cssSelector("#email"), user.getEmail());
-        type(By.cssSelector("#password"), user.getPassword());
     }
 
 }
--- 21_06_08_ilCarro/src/main/java/com/ilCarro/qa14/TestBase.java
+++ 21_06_08_ilCarro/src/main/java/com/ilCarro/qa14/TestBase.java
@@ -17,46 +17,49 @@ public class TestBase {
 
     @BeforeSuite
     public void setUp() {
+        //System.setProperty("webdriver.chrome.driver","path/chromedriver.exe");
         wd = new ChromeDriver();
         wd.get("https://ilcarro-dev-v1.firebaseapp.com/");
         wd.manage().window().maximize();
-        wd.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod(enabled = false)
+    public void tearDown() {
+        wd.quit();
     }
 
     public boolean isElementPresent(By by) {
-        return wd.findElements(by).size()>0;
+        return wd.findElements(by).size() > 0;
     }
 
     public boolean isElementPresent1(By locator) {
-        try{
+        try {
             wd.findElement(locator);
             return true;
-        }catch (NoSuchElementException ex){
+        } catch (NoSuchElementException ex) {
             return false;
         }
-    }
-
-    @AfterMethod(enabled = true)
-    public void tearDown() {
-        wd.quit();
     }
 
     public void logout() {
         click(By.xpath("//a[contains(.,'logOut')]"));
     }
 
-    public boolean isSignUpFormPresent() {
+    public boolean isSignUpButtonPresent() {
         return isElementPresent(By.cssSelector("[href='/signup']"));
     }
 
-    public void isLoginFormPresent() {
+    public void isSignUpFormPresent() {
         Assert.assertTrue(isElementPresent(By.cssSelector("form.signup__fields")));
     }
 
     public void type(By locator, String text) {
-        click(locator);
-        wd.findElement(locator).clear();
-        wd.findElement(locator).sendKeys(text);
+        if (text!=null) {
+            click(locator);
+            wd.findElement(locator).clear();
+            wd.findElement(locator).sendKeys(text);
+        }
     }
 
     public void click(By locator) {
@@ -72,7 +75,70 @@ public class TestBase {
     }
 
     public void jumpToFooter() {
-        wd.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL,Keys.END);
+        wd.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL, Keys.END);
+    }
+
+    public boolean isFindCarFormPresent() {
+        return isElementPresent1(By
+                .cssSelector(".Main_mainpage__find_your_car__AHLkw"));
+    }
+
+    public void clickOnLoginTab() {
+        click(By.xpath("//ul[@class='header__nav desktop']/li[5]"));
+    }
+
+    public void fillLoginForm(User user) {
+        type(By.cssSelector("[name='email']"), user.getEmail());
+        type(By.name("password"), user.getPassword());
+    }
+
+    public void logIn() {
+        clickOnLoginTab();
+        type(By.cssSelector("[name='email']"),"hummels@gmail.com");
+        type(By.name("password"),"Hummels098765");
+        submit();
+    }
+
+    public boolean isUserLoggedIn() {
+        return isElementPresent(By.xpath("//a[contains(.,'logOut')]"));
+    }
+
+    public void pause() throws InterruptedException {
+        Thread.sleep(2000);
+    }
+
+    public void clickOnAddCarTab() {
+        click(By.xpath("//ul[@class='header__nav desktop']/li[2]"));
+    }
+
+    public void fillCarForm(Car car) {
+        type(By.cssSelector(".country"), car.getCountry());
+        type(By.cssSelector(".address"), car.getAddress());
+        type(By.cssSelector(".distance_included"), car.getDistance());
+    }
+
+    public void isLogInFormPresent() {
+        Assert.assertTrue(isElementPresent(By.cssSelector(".Login_login__right_block__1niYm")));
+    }
+
+    public void clickCheckPolicy() {
+        click(By.cssSelector("#check_policy"));
+    }
+
+    public void clickOnSignUpTab() {
+        click(By.cssSelector("[href='/signup']"));
+    }
+
+    public void fillRegistrationForm(User user) {
+        type(By.cssSelector("#first_name"), user.getFirstName());
+        type(By.cssSelector("#second_name"), user.getSecondName());
+        type(By.cssSelector("#email"), user.getEmail());
+        type(By.cssSelector("#password"), user.getPassword());
+    }
+
+    public void checkSearchPageLounch() {
+        click(By.xpath("//ul[@class='header__nav desktop']/li[1]"));
+        Assert.assertTrue(isElementPresent1(By.cssSelector(".search-sidebar__content")));
     }
 
 }
