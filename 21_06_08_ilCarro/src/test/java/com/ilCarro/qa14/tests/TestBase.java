71fe0fe9067cf5699956c63858424487395b2c92
enextus <enextus@gmail.com>
2021-06-16 12:24:55 +0200

    21_06_16_upd

--- 21_06_08_ilCarro/src/test/java/com/ilCarro/qa14/tests/TestBase.java
+++ 21_06_08_ilCarro/src/test/java/com/ilCarro/qa14/tests/TestBase.java
@@ -2,19 +2,21 @@ package com.ilCarro.qa14.tests;
 
 import com.ilCarro.qa14.fw.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.log4testng.Logger;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class TestBase {
 
-    protected static ApplicationManager app = new ApplicationManager(System
-            .getProperty("browser", BrowserType.CHROME));
+    protected static ApplicationManager app = new ApplicationManager(System.getProperty(
+                                                                                    "browser",
+                                                                                         BrowserType.CHROME));
 
     Logger logger = LoggerFactory.getLogger(TestBase.class);
 
