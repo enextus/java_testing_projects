aa6bcafe3e6413d10a7759d0cc1b7de4c18ea0b3
enextus <enextus@gmail.com>
2021-06-27 19:55:56 +0200

    21_06_27_QA14_ilCarro-main

---
+++ 21_06_16_QA14_ilCarro-main/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+G
+src/main/java/Test.java,b/6/b660a5f333c00ef78dec1d85c727eb000157c1f3
--- 21_06_16_QA14_ilCarro-main/src/test/java/com/ilCarro/qa14/tests/CreateAccountTest.java
+++ 21_06_16_QA14_ilCarro-main/src/test/java/com/ilCarro/qa14/tests/CreateAccountTest.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+
 public class CreateAccountTest extends TestBase {
 
     @BeforeMethod
