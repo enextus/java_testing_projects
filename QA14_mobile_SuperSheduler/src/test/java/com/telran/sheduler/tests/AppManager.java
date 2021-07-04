47f0c70694ae744b984be7eabbaed1d7d784d38e
enextus <enextus@gmail.com>
2021-07-04 22:32:21 +0200

    21_07_04_

---
+++ QA14_mobile_SuperSheduler/src/test/java/com/telran/sheduler/tests/AppManager.java
@@ -0,0 +1,33 @@
+package com.telran.sheduler.tests;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AppManager {
+
+
+    AppiumDriver driver;
+    DesiredCapabilities capabilities;
+
+    public void init() throws MalformedURLException {
+        capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "8.0.0");
+        capabilities.setCapability("deviceName", "qa14_mob");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "com.example.svetlana.scheduler");
+        capabilities.setCapability("appActivity", ".presentation.splashScreen.SplashScreenActivity");
+        capabilities.setCapability("app", "/home/enextus/Desktop/v.0.0.3.apk");
+
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub/"), capabilities);
+    }
+
+    public void stop() {
+        driver.quit();
+    }
+}
--- QA14_mobile_SuperSheduler/src/test/java/com/telran/sheduler/tests/TestBase.java
+++ QA14_mobile_SuperSheduler/src/test/java/com/telran/sheduler/tests/TestBase.java
@@ -9,27 +9,16 @@ import org.testng.annotations.BeforeMethod;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class TestBase {
-    AppiumDriver driver;
-    DesiredCapabilities capabilities;
+public class TestBase extends AppManager{
 
     @BeforeMethod
     public void setUp() throws MalformedURLException {
-        capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "8.0.0");
-        capabilities.setCapability("deviceName", "qa14_mob");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "com.example.svetlana.scheduler");
-        capabilities.setCapability("appActivity", ".presentation.splashScreen.SplashScreenActivity");
-        capabilities.setCapability("app", "/home/enextus/Desktop/v.0.0.3.apk");
-
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub/"), capabilities);
+        init();
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.quit();
+        stop();
     }
+
 }
