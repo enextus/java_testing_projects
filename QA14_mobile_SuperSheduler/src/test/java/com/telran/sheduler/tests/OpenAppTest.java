dcb159598ff838815c76e58fb70d2570c574dbe2
enextus <enextus@gmail.com>
2021-07-04 22:16:35 +0200

    21_07_04_

--- QA14_mobile_SuperSheduler/src/test/java/com/telran/sheduler/tests/OpenAppTest.java
+++ QA14_mobile_SuperSheduler/src/test/java/com/telran/sheduler/tests/OpenAppTest.java
@@ -3,31 +3,15 @@ package com.telran.sheduler.tests;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
-public class OpenAppTest {
-
-    AppiumDriver driver;
-    DesiredCapabilities capabilities;
-
-    @BeforeMethod
-    public void setUp() throws MalformedURLException {
-        capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("platformVersion", "8.0.0");
-        capabilities.setCapability("deviceName", "qa14_mob");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "com.example.svetlana.scheduler");
-        capabilities.setCapability("appActivity", ".presentation.splashScreen.SplashScreenActivity");
-        capabilities.setCapability("app", "/home/enextus/Desktop/v.0.0.3.apk");
-
-        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub/"), capabilities);
-    }
+public class OpenAppTest extends TestBase {
 
     /*{
     "platformName": "Android",
@@ -39,9 +23,10 @@ public class OpenAppTest {
 
     @Test
     public void testLaunchApp() {
+        List<AppiumDriver> ad = driver.findElementsById("com.example.svetlana.scheduler:id/splash_logo_img");
 
-        driver.findElementsById("com.example.svetlana.scheduler:id/splash_logo_img");
-
+        System.out.println(ad.toString());
+        System.out.println("_____________________________");
         // Assert.assertTrue(isElementPresent(By.cssSelector("form.signup__fields")));
     }
 
