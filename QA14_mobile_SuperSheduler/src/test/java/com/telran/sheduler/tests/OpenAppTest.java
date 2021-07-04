ed4eb5317b7b6a996bdc0f1175adcdc0479432ec
enextus <enextus@gmail.com>
2021-07-04 21:01:33 +0200

    21_07_04_

--- QA14_mobile_SuperSheduler/src/test/java/com/telran/sheduler/tests/OpenAppTest.java
+++ QA14_mobile_SuperSheduler/src/test/java/com/telran/sheduler/tests/OpenAppTest.java
@@ -1,4 +1,31 @@
 package com.telran.sheduler.tests;
 
+import io.appium.java_client.AppiumDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 public class OpenAppTest {
+
+    AppiumDriver ad;
+
+    DesiredCapabilities capabilities;
+
+    @BeforeMethod
+    public void setUp() {
+        capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("platformVersion", "8.0.0");
+        capabilities.setCapability("deviceName", "qa14_mob");
+        capabilities.setCapability("appPackage", "com.example.svetlana.scheduler");
+        capabilities.setCapability("appActivity", ".presentation.splashScreen.SplashScreenActivity");
+        // capabilities.setCapability("platformName", "Android");
+    }
+
+    /*{
+  "platformName": "Android",
+  "platformVersion": "8.0.0",
+  "deviceName": "qa14_mob",
+  "appPackage": "com.example.svetlana.scheduler",
+  "appActivity": ".presentation.splashScreen.SplashScreenActivity"
+}*/
 }
