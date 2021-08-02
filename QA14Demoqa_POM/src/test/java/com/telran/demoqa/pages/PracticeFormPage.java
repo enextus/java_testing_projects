4c2730d47c598445da2294b7184467fca5c4ad34
enextus <enextus@gmail.com>
2021-08-02 18:27:42 +0200

    21_08_02___

--- QA14Demoqa_POM/src/test/java/com/telran/demoqa/pages/PracticeFormPage.java
+++ QA14Demoqa_POM/src/test/java/com/telran/demoqa/pages/PracticeFormPage.java
@@ -1,5 +1,6 @@
 package com.telran.demoqa.pages;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,7 +10,20 @@ public class PracticeFormPage extends PageBase {
         super(driver);
     }
 
-    public PracticeFormPage fillPersonalData(String name, String lastName, String email, String number) {
+    @FindBy(id = "firstName")
+    WebElement firstName;
+    @FindBy(id = "lastName")
+    WebElement lastName;
+    @FindBy(id = "userEmail")
+    WebElement userEmail;
+    @FindBy(id = "userNumber")
+    WebElement mobilNumber;
+
+    public PracticeFormPage fillPersonalData(String fName, String lName, String email, String number) {
+        type(firstName, fName);
+        type(lastName, lName);
+        type(userEmail, email);
+        type(mobilNumber, number);
 
         return this;
     }
@@ -23,7 +37,6 @@ public class PracticeFormPage extends PageBase {
     @FindBy(xpath = "//label[@for='gender-radio-3']")
     WebElement otherBtn;
 
-
     public PracticeFormPage selectGender(String gender) {
         if (gender.equals("Male")) {
             maleBtn.click();
@@ -36,5 +49,136 @@ public class PracticeFormPage extends PageBase {
         return this;
     }
 
-    
+    @FindBy(id = "dateOfBirthInput")
+    WebElement dateOfBirthInput;
+
+    public PracticeFormPage typeOfDay(String bDay) {
+        clickWithAction(dateOfBirthInput, 0, 200);
+        String os = System.getProperty("os.name");
+        System.out.println(os);
+
+        if (os.startsWith("Mac")) {
+            dateOfBirthInput.sendKeys(Keys.chord(Keys.COMMAND, "a"));
+        } else {
+            dateOfBirthInput.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        }
+
+        dateOfBirthInput.sendKeys(bDay);
+
+        dateOfBirthInput.sendKeys(Keys.ENTER);
+
+        return this;
+    }
+
+    @FindBy(id = "subjectsInput")
+    WebElement subjectInput;
+
+    @FindBy(id = "react-select-2-option-0")
+    WebElement selectInput;
+
+    public PracticeFormPage addSubject(String subject) {
+        type(subjectInput, subject);
+        selectInput.click();
+
+        return this;
+    }
+
+    public PracticeFormPage addSubjectWithEnter(String subject) {
+        type(subjectInput, subject);
+        subjectInput.sendKeys(Keys.ENTER);
+
+        return this;
+    }
+
+    @FindBy(xpath = "//label[@for='hobbies-checkbox-1']")
+    WebElement sport;
+    @FindBy(xpath = "//label[@for='hobbies-checkbox-2']")
+    WebElement reading;
+    @FindBy(xpath = "//label[@for='hobbies-checkbox-3']")
+    WebElement music;
+
+    public PracticeFormPage chooseHobby(String hobby) {
+        if (hobby.equals("Sports")) {
+            sport.click();
+        } else if (hobby.equals("Reading")) {
+            reading.click();
+        } else if (hobby.equals("Music")) {
+            music.click();
+        }
+
+        return this;
+    }
+
+    @FindBy(xpath = "//input[@id='uploadPicture']")
+    WebElement chooseFile;
+
+    public PracticeFormPage uploadFile(String path) {
+        chooseFile.sendKeys(path);
+        pause(2000);
+
+        return this;
+    }
+
+    @FindBy(id = "currentAddress")
+    WebElement address;
+
+    public PracticeFormPage typeAddress(String text) {
+        typeWithAction(address, 0, 300, text);
+
+        return this;
+    }
+
+    @FindBy(id = "state")
+    WebElement state;
+    @FindBy(id = "react-select-3-input")
+    WebElement stateBlock;
+
+    public PracticeFormPage inputState(String st) {
+        state.click();
+        stateBlock.sendKeys(st);
+        stateBlock.sendKeys(Keys.ENTER);
+
+        return this;
+    }
+
+    @FindBy(id = "city")
+    WebElement city;
+    @FindBy(id = "react-select-4-input")
+    WebElement cityBlock;
+
+    public PracticeFormPage inputCity(String c) {
+        city.click();
+        cityBlock.sendKeys(c);
+        cityBlock.sendKeys(Keys.ENTER);
+
+        return this;
+    }
+
+    @FindBy(id = "submit")
+    WebElement submit;
+
+    public PracticeFormPage submit() {
+        submit.click();
+
+        return this;
+    }
+
+    @FindBy(id = "example-modal-sizes-title-lg")
+    WebElement modalTitle;
+
+    public String getTitleFromDialog() {
+
+        return modalTitle.getText();
+    }
+
+    @FindBy(id = "closeLargeModal")
+    WebElement closeBtn;
+
+    public PracticeFormPage closeSuccessDialog() {
+        closeBanner();
+        closeBtn.click();
+
+        return this;
+    }
+
 }
