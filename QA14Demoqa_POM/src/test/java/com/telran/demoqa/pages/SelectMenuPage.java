31136ee1bf42962d7110f7b858227465c92966c5
enextus <enextus@gmail.com>
2021-08-02 18:28:20 +0200

    21_08_02___

--- QA14Demoqa_POM/src/test/java/com/telran/demoqa/pages/SelectMenuPage.java
+++ QA14Demoqa_POM/src/test/java/com/telran/demoqa/pages/SelectMenuPage.java
@@ -1,6 +1,5 @@
 package com.telran.demoqa.pages;
 
-import com.telran.demoqa.tests.TestBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
