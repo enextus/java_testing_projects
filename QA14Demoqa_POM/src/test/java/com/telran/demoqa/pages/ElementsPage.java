49add8e7f32ef899124fb5de8aecdbdb70fc18bf
enextus <enextus@gmail.com>
2021-08-09 22:58:23 +0200

    21_08_09___

--- QA14Demoqa_POM/src/test/java/com/telran/demoqa/pages/ElementsPage.java
+++ QA14Demoqa_POM/src/test/java/com/telran/demoqa/pages/ElementsPage.java
@@ -2,7 +2,9 @@ package com.telran.demoqa.pages;
 
 import org.openqa.selenium.WebDriver;
 
-public class ElementsPage {
+public class ElementsPage extends PageBase {
     public ElementsPage(WebDriver driver) {
+        super(driver);
     }
+
 }
