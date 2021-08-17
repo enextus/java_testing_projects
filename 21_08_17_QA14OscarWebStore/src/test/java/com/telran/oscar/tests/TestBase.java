11daf600c679a8aaf7c462bfc513fa70856bd578
enextus <enextus@gmail.com>
2021-08-17 12:54:24 +0200

    21_08_17_

--- 21_08_17_QA14OscarWebStore/src/test/java/com/telran/oscar/tests/TestBase.java
+++ 21_08_17_QA14OscarWebStore/src/test/java/com/telran/oscar/tests/TestBase.java
@@ -1,5 +1,6 @@
 package com.telran.oscar.tests;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
@@ -10,38 +11,14 @@ import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
-    protected EventFiringWebDriver driver;
+    WebDriver driver;
 
     @BeforeMethod
     public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-
-        System.setProperty("webdriver.chrome.args", "--disable-logging");
-        System.setProperty("webdriver.chrome.silentOutput", "true");
-
-        options.setBinary("/usr/bin/google-chrome-stable"); // path to our chrome binary
-
-        options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-        options.addArguments("disable-infobars"); // disabling infobars
-        options.addArguments("--disable-extensions"); // disabling extensions
-        options.addArguments("--disable-gpu"); // applicable to windows os only
-        options.addArguments("window-size=1024,768"); // Bypass OS security model
-        options.addArguments("--log-level=3"); // set log level
-        options.addArguments("--silent");//
-        options.setCapability("chrome.verbose", false); //disable logging
-
-        // With options
-        // driver = new EventFiringWebDriver(new ChromeDriver(options));
-
-        // Without options
-        driver = new EventFiringWebDriver(new ChromeDriver());
-
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        driver.get("https://demoqa.com/");
+        driver.get("http://selenium1py.pythonanywhere.com/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @AfterMethod(enabled = false)
