a9face3d08cf2691fd0b290cfd017f4994ac16f3
enextus <enextus@gmail.com>
2021-08-17 12:54:37 +0200

    21_08_17_

--- 21_08_17_QA14OscarWebStore/src/test/java/com/telran/oscar/tests/TestBase.java
+++ 21_08_17_QA14OscarWebStore/src/test/java/com/telran/oscar/tests/TestBase.java
@@ -2,8 +2,6 @@ package com.telran.oscar.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
