50d10eeacb68b8ab19953787589ecc260de416a6
enextus <enextus@gmail.com>
2021-08-17 12:52:02 +0200

    21_08_17_

--- 21_08_17_QA14OscarWebStore/src/test/java/com/telran/oscar/pages/HomePage.java
+++ 21_08_17_QA14OscarWebStore/src/test/java/com/telran/oscar/pages/HomePage.java
@@ -1,6 +1,22 @@
 package com.telran.oscar.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+
 public class HomePage extends PageBase {
 
-    
+    public HomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(css = ".dropdown-submenu")
+    WebElement submenuBook;
+
+    public ProductPage selectCategory() {
+        submenuBook.click();
+        return new ProductPage(driver);
+    }
+
 }
