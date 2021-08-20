27e93e0005457d51d62bbd12878f84196b595a2e
enextus <enextus@gmail.com>
2021-08-20 08:23:51 +0200

    21_08_20_

--- 21_08_17_QA14OscarWebStore/src/test/java/com/telran/oscar/tests/TestBase.java
+++ 21_08_17_QA14OscarWebStore/src/test/java/com/telran/oscar/tests/TestBase.java
@@ -2,14 +2,31 @@ package com.telran.oscar.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.lang.reflect.Method;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
+
 
 public class TestBase {
 
     WebDriver driver;
+
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
+    @BeforeMethod(alwaysRun = true)
+    public void startTest(Method method, Object[] parameters) {
+//        logger.info("Start test: " + method.getName());
+//        if (p.length!=0) {
+//            logger.info("--> With data: " + Arrays.asList(p));
+//        }
+        logger.info("Start test " + method.getName() + " with data: " + Arrays.asList(parameters));
+    }
+
 
     @BeforeMethod
     public void setUp() {
@@ -19,7 +36,7 @@ public class TestBase {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
-    @AfterMethod(enabled = false)
+    @AfterMethod(enabled = true)
     public void tearDown() {
         driver.quit();
     }
